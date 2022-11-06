3190115fe66873055be00ebb3e19bc546b74f1ed
kimmingyu <minku4820@gmail.com>
2022-11-06 23:23:40 +0900

    Paint Color

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const COLOROPTION = Array.from(document.getElementsByClassName("color-option"));
+const COLOR = document.querySelector("#color");
 const LINEWIDTH = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -24,9 +26,24 @@ const cancelPainting = (event) => {
 const onLineWidthChange = (event) => {
   ctx.lineWidth = event.target.value;
 };
+const onColorChange = (event) => {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+};
+
+const onColorClick = (event) => {
+  const colorValue = event.target.dataset.color;
+  console.dir(colorValue);
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  COLOR.value = colorValue;
+};
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 LINEWIDTH.addEventListener("change", onLineWidthChange);
+COLOR.addEventListener("change", onColorChange);
+
+COLOROPTION.forEach((color) => color.addEventListener("click", onColorClick));
