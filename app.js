09e69018743f1238c104d93e8a84c53b5ee3e718
kimmingyu <minku4820@gmail.com>
2022-11-06 23:06:02 +0900

    Line Width 조절

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const LINEWIDTH = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = LINEWIDTH.value;
 let isPainting = false;
 const onMove = (event) => {
   if (isPainting) {
@@ -16,10 +18,15 @@ const startPainting = (event) => {
   isPainting = true;
 };
 const cancelPainting = (event) => {
+  ctx.beginPath();
   isPainting = false;
+};
+const onLineWidthChange = (event) => {
+  ctx.lineWidth = event.target.value;
 };
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+LINEWIDTH.addEventListener("change", onLineWidthChange);
